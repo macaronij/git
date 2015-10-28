388369aac3c7724a69d593e4cb6bfafe8df2087b
Juan Francisco <macaronij@gmail.com>
2015-10-27 21:57:11 -0300

    primera funcion

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,10 +2,22 @@ import java.util.Scanner;
 
 public class HelloWorld
 {
+	private static Scanner clavier = new Scanner(System.in);
+	
 	public static void main (String[] args)
 	{
-		Scanner clavier = new Scanner(System.in);
-
 		System.out.println("ejem");
+		int a=5;
+		int d = Doub();
+		System.out.println(d);
+	}
+	
+	static int Doub()
+	{
+		System.out.print("ingrese: ");
+		int b = clavier.nextInt();
+		int c = b*2;
+		
+		return c;
 	}
 }
